15ab28d408253159cebe3a3d5a81c6282fc5be30
igor.sadza <igor.sadza@gmail.com>
2026-08-05 19:56:24 +0000

    cicd: added act.sh script

--- .cicd/github/act.sh
+++ .cicd/github/act.sh
@@ -2,10 +2,17 @@
 # https://nektosact.com/usage/index.html
 
 # ----
+<<<<<<< HEAD
+#cat << EOF > /tmp/act_secrets
+#GITHUB_TOKEN=""
+#TOKEN=""
+#EOF
+=======
 cat << EOF > /tmp/act_secrets
 GITHUB_TOKEN=""
 TOKEN=""
 EOF
+>>>>>>> c331b14 (Revert "cicd: added act.sh script")
 # ----
 
 act \
