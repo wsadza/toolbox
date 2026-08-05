5eb4717a1469a117c0904d8baf86d1fcf04d39ec
igor.sadza <igor.sadza@gmail.com>
2026-08-05 19:58:04 +0000

    fix: corrected act.sh

--- .cicd/github/act.sh
+++ .cicd/github/act.sh
@@ -2,17 +2,10 @@
 # https://nektosact.com/usage/index.html
 
 # ----
-<<<<<<< HEAD
 #cat << EOF > /tmp/act_secrets
 #GITHUB_TOKEN=""
 #TOKEN=""
 #EOF
-=======
-cat << EOF > /tmp/act_secrets
-GITHUB_TOKEN=""
-TOKEN=""
-EOF
->>>>>>> c331b14 (Revert "cicd: added act.sh script")
 # ----
 
 act \
